1c93f1e96fd91a625ea2ca9f083342ed121b90bf
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-14 15:53:21 +0100

    Add AppHeader Component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<template>
+    <h1 class="text-white py-3 px-3">Breaking Bad Api</h1>
+</template>
+
+<style lang="scss">
+
+</style>
